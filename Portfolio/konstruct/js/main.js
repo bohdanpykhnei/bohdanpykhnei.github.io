ecbaba457241c1bac88a89d13a731257a4096a6b
Bohdan <90638793+bohdanpykhnei@users.noreply.github.com>
2023-03-20 21:50:09 +0200

    Add files via upload

--- Portfolio/konstruct/js/main.js
+++ Portfolio/konstruct/js/main.js
@@ -1,14 +1,37 @@
 const button = document.querySelector("#burger-menu_button");
 const sideBar = document.querySelector("#menu");
 const burgerBtn = document.querySelector("#burger-menu_button");
-const menuLink = document.querySelector(".menu__list");
-const body = document.body;
 
-button.addEventListener("click", buttonHandler);
+button.addEventListener("click", open);
+sideBar.addEventListener("click", close);
 
-function buttonHandler(e) {
+function open(e) {
   e.preventDefault();
   sideBar.classList.toggle("menu__active");
   burgerBtn.classList.toggle("button__active");
-  body.classList.toggle("noscroll");
+  document.body.classList.toggle("noscroll");
 }
+function close(e) {
+  if (e.target.matches('a')) {
+    sideBar.classList.remove("menu__active");
+    burgerBtn.classList.remove("button__active");
+    document.body.classList.remove("noscroll");
+  }
+}
+// ==================================
+
+const toTopBtn = document.querySelector(".back-to-top");
+
+window.onscroll = function () {
+  if (window.pageYOffset > 580) {
+    toTopBtn.style.bottom = "40px"
+  } else {
+    toTopBtn.style.bottom = "-40px"
+  }
+}
+// плавний скрол вверх
+toTopBtn.addEventListener("click", function () {
+  window.scrollBy({
+    top: -document.documentElement.scrollHeight
+  });
+});
